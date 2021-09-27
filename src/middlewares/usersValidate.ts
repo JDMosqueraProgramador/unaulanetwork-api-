ce7f9b78ff268c6d6bef9cdc5882a8216776200b
Alexander Rendon Londoño <rendona299@gmail.com>
2021-09-27 17:27:21 -0500

    Validar que el usuario siga almenos a una persona cuando inicia su sesion por primera vez

--- src/middlewares/usersValidate.ts
+++ src/middlewares/usersValidate.ts
@@ -1,76 +1,90 @@
 import mongoose from 'mongoose';
-const {body, checkSchema, validationResult} = require('express-validator');
+const { body, checkSchema, validationResult } = require('express-validator');
 import User from '../models/users.models';
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { validate } from 'uuid';
 
 
 const userSchemaValidator = checkSchema({
 
-        username:{
-            
-            custom : {
-                options: async(value:any) =>{
+    username: {
 
-                    try{
-                        const user = await User.findOne({"username": value})
-        
-                        if(user!=null){
+        custom: {
+            options: async (value: any) => {
+
+                try {
+                    const user = await User.findOne({ "username": value })
+
+                    if (user != null) {
                         //console.log("Acá se verificó que NO se creó")
                         throw new Error('Este username está ocupado');
 
-                        }
-                    }catch{
-                        throw new Error('No has enviado correctamente el nombre de usuario')
                     }
-                    
+                } catch {
+                    throw new Error('No has enviado correctamente el nombre de usuario')
                 }
-            },   
-        },
-        dayOfBirth :{
-            custom:{
-                options: async(value:any)=>{
-                    try{
-                        const birth = value;
-                        const validate = new Date((birth)).getFullYear();
-                        const dateToday = new Date().getFullYear() - validate;
-                    
-                        if (dateToday <= 14) {
 
-                            throw new Error('Fecha inválida');
-                        }
-                    } catch{
-                        throw new Error('No has enviado correctamente la fecha de nacimiento')
-                    }
-                }
             }
         },
-        description: {
-            custom:{
+    },
+    dayOfBirth: {
+        custom: {
+            options: async (value: any) => {
+                try {
+                    const birth = value;
+                    const validate = new Date((birth)).getFullYear();
+                    const dateToday = new Date().getFullYear() - validate;
 
-               
-                options: async(value:any) =>{
-                    try{ 
-                     if(value.length < 12){
-                       throw new Error('Descripción demasiado corta')
-                     }
-                     else if(value.length > 100) {
+                    if (dateToday <= 14) {
 
-                         throw new Error('Descripción demasiado larga ')
-                     }
-                    } catch{
+                        throw new Error('Fecha inválida');
+                    }
+                } catch {
+                    throw new Error('No has enviado correctamente la fecha de nacimiento')
+                }
+            }
+        }
+    },
+    description: {
+        custom: {
+
+
+            options: async (value: any) => {
+                try {
+                    if (value.length < 12) {
+                        throw new Error('Descripción demasiado corta')
+                    }
+                    else if (value.length > 100) {
+
+                        throw new Error('Descripción demasiado larga')
+                    }
+                } catch {
                     throw new Error('No has enviado correctamente la descripción')
                 }
             }
-               
-                
+
+        }
+    },
+    following: {
+
+        custom: {
+
+            options: async (value: any) => {
+
+                try {
+                    if (value.length < 1) {
+
+                        throw new Error('Debes seguir almenos una persona.')
+                    }
+                } catch {
+                    throw new Error('Se debe seguir almenos a una persona.')
+                }
 
             }
         }
-      
     }
 
-)
+});
 
 
 export default userSchemaValidator;
