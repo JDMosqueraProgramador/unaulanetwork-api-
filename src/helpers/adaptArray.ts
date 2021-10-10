1c3b4c2cffeafc2aecf05b4cc91a9d7bc9f9ef62
Dylanyessid <dylanyessid1904@gmail.com>
2021-10-10 09:55:46 -0500

    arreglar arreglos para enviar correctamente

--- src/helpers/adaptArray.ts
+++ src/helpers/adaptArray.ts
@@ -3,19 +3,18 @@ import {Request, Response} from 'express'
 
 export const fixArrays = (req:Request, res:Response, next:any) =>{
 
-    console.log(req.body)
+    
 
     let followingArray=[];
     let achievementArray=[]
 
-    if(typeof req.body.achievement == 'string'){
+    if(typeof req.body.achievement == 'string' && req.body.achievement != ""){
         achievementArray.push(req.body.achievement);
         req.body.achievement = achievementArray;
     }
     
-    if (typeof req.body.following == "string") {
-        followingArray.push(req.body.following)
-        req.body.following = followingArray;
-    }
+    
+
+    console.log(req.body);
     next();
 }
